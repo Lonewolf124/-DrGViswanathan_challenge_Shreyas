4e0b0be90df306cf87a604e08302d217140e958d
SHREYAS DESAI <145777054+Lonewolf124@users.noreply.github.com>
2024-11-21 18:32:39 +0530

    Create solution.cpp

---
+++ Day_54/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+
+   
+    void rotateArr(vector<int>& arr, int d) {
+        int n = arr.size();
+        d = d % n; 
+        
+        reverse(arr.begin(), arr.begin() + d);      
+        reverse(arr.begin() + d, arr.end());         
+        reverse(arr.begin(), arr.end());  
+    }
+};
